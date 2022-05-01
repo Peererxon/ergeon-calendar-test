04b0da84bbd83018812267825859179ba5174de6
anderson gil <anderson.gil@karibu.cl>
2022-05-01 12:42:23 -0400

    WIP: slider with slick centered
    - Installing library
    - Set settings to basic slider with 7 items

---
+++ src/components/SlickCarousel.tsx
@@ -0,0 +1,60 @@
+import Slider, { Settings } from "react-slick";
+export const SlockCarousel = () => {
+    const settings: Settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 7,
+      slidesToScroll: 7,
+      arrows: true,
+    };
+    return (
+      <div className="container">
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>          
+                </div>
+                <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>          
+          </div>
+        </Slider>
+      </div>
+    );
+}
