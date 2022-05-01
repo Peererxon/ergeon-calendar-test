3babfa985433ccedd28aa3fe9acc087bead1368b
anderson gil <anderson.gil@karibu.cl>
2022-05-01 16:26:57 -0400

    WIP: Check if item is actual day
    - Validating and sending info to prop
    - Showing little dot in case that is actual day

--- src/components/CalendarItem.tsx
+++ src/components/CalendarItem.tsx
@@ -2,9 +2,16 @@
 import { CalendarDay } from '../interfaces/CalendarDay';
 import './styles/CalendarItem.css';
 
-export const CalendarItem = ({ dayName, dayNumber }: CalendarDay ) => {
+interface Props extends CalendarDay {
+    isActualDay?: boolean,
+    isSelected?: boolean,
+    
+}
+
+export const CalendarItem = ({ dayName, dayNumber, isActualDay }: Props ) => {
   return (
     <div className="calendarDay">
+        { isActualDay && <div className='calendarDay__today'></div> }
         <span className="calendarDay__dayTitle">
             <time> {dayName} </time>
         </span>
--- src/container/Calendar.tsx
+++ src/container/Calendar.tsx
@@ -4,39 +4,47 @@ import { CalendarDay } from "../interfaces/CalendarDay";
 import { CalendarItem } from "../components/CalendarItem";
 import { getFirstDayOfMonth } from "../utils/firstDayMonth";
 import './styles/Calendar.css';
+const date = new Date();
+const firstDayCurrentMonth = getFirstDayOfMonth(
+  date.getFullYear(),
+  date.getMonth(),
+);
+//const result = getDaysInMonth(new Date()) // number of days in the actual month
+const lastDay = parseInt(format(lastDayOfMonth(date), 'd')) 
+const actualMonthDays: CalendarDay[] = []
+const todayFormatted = format(date, 'EEE/d') // Sun/1
+const actualMonth = format(date, 'MMMM')
+
+for (let dayIndex = 0; dayIndex < lastDay; dayIndex++) {
+    //const day = format(addDays(firstDayCurrentMonth, dayIndex), 'EEE/d/MMMM') // Sun/1/May
+    const day: CalendarDay = {
+        dayName: format(addDays(firstDayCurrentMonth, dayIndex), 'EEE'), // Sun
+        dayNumber: format(addDays(firstDayCurrentMonth, dayIndex), 'd') // 1
+    }      
+    actualMonthDays.push(day)
+}
 export const Calendar = () => {
-    const date = new Date();
-    const firstDayCurrentMonth = getFirstDayOfMonth(
-    date.getFullYear(),
-    date.getMonth(),
-    );
-    const todayNumber = parseInt( format( date, 'e' ) )
-    //const result = getDaysInMonth(new Date()) // number of days in the actual month
-    // new
-    const lastDay = parseInt(format(lastDayOfMonth(date), 'd')) 
-    const actualMonthDays = [  ]    
-    for (let dayIndex = 0; dayIndex < lastDay; dayIndex++) {
-        //const day = format(addDays(firstDayCurrentMonth, dayIndex), 'EEE/d/MMMM') // Sun/1/May
-        const day: CalendarDay = {
-            dayName: format(addDays(firstDayCurrentMonth, dayIndex), 'EEE'),
-            dayNumber: format(addDays(firstDayCurrentMonth, dayIndex), 'd')
-        }
-        actualMonthDays.push(day)
-    }
     const settings: Settings = {
       dots: false,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 7,
       slidesToScroll: 7,
+      rows: 1,
       arrows: true,
     };
     return (
       <div className="container">
-        <h2> Single Item</h2>
+        <h2> {actualMonth}</h2>
         <Slider {...settings}>
             {
-                    actualMonthDays.map((day, index) => <CalendarItem {...day} key={ day.dayName + day.dayNumber }/> )
+              actualMonthDays.map((day, index) => (
+                <CalendarItem
+                  {...day}
+                  key={`${day.dayName}/${day.dayNumber}`}
+                  isActualDay={ todayFormatted === (`${day.dayName}/${day.dayNumber}`) }
+                />
+              ))
             }
         </Slider>
       </div>
